bdc70aced20a26a0003f2beccfc30c91f9726c03
haimlm <105680265+haimlm@users.noreply.github.com>
2025-03-07 15:43:03 +0200

    fixes

--- data-updates.lua
+++ data-updates.lua
@@ -13,22 +13,31 @@ for mname, machine_orig in pairs(data.raw["assembling-machine"]) do
                     -- how am I supposed to figure out what item places this
                     -- if anyone has another idea, make a PR and fix and cleanup the code yourself
                     -- man it's like holoitems again
+
                     machine.placeable_by = nil
                 elseif machine_orig.minable.result ~= nil then
                     machine.placeable_by = {item=machine_orig.minable.result, count=1, quality=qvalue}
+                elseif machine_orig.minable.results ~= nil and type(machine_orig.minable.results) == "table" and #machine_orig.minable.results > 0 and machine_orig.minable.results[1].name ~= nil then
+                    machine.placeable_by = {item=machine_orig.minable.results[1].name, count=1, quality=qvalue}
                 else
-                    -- true for most machines tbh, actually most machines dont even use more than one results[] value
-                    machine.placeable_by = {item=machine_orig.minable.results[1]["name"], count=1, quality=qvalue}
+                -- true for most machines tbh, actually most machines dont even use more than one results[] value
+                    machine.placeable_by = nil
                 end
-            elseif machine_orig.placeable_by["item"] ~= nil then
-                machine.placeable_by = table.deepcopy(machine_orig.placeable_by)
-                machine.placeable_by["quality"]=qvalue
-            else
-                machine.placeable_by = table.deepcopy(machine_orig.placeable_by)
-                for idx,value in pairs(machine_orig.placeable_by) do
-                    machine.placeable_by[idx]["quality"] = qvalue
+            elseif type(machine_orig.placeable_by) == "table" then
+                if machine_orig.placeable_by["item"] ~= nil then
+                    machine.placeable_by = table.deepcopy(machine_orig.placeable_by)
+                    machine.placeable_by["quality"] = qvalue
+                else
+                    machine.placeable_by = table.deepcopy(machine_orig.placeable_by)
+                    for idx, value in ipairs(machine_orig.placeable_by) do
+                        if type(value) == "table" and value["item"] ~= nil then
+                            machine.placeable_by[idx]["quality"] = qvalue
+                        end
+                    end
                 end
                 log(tostring(machine.placeable_by[1]["item"]))
+            else
+                machine.placeable_by = nil
             end
             machine.localised_name = {"entity-name." .. mname}
             machine.localised_description = {"entity-description." .. mname}
